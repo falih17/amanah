6a2fb978720594385f786319f0101db02072fc22
Adi Falih <spidey354@gmail.com>
2025-06-17 08:32:38 +0700

    ok bos

--- application/modules/front_page/views/v_player2.php
+++ application/modules/front_page/views/v_player2.php
@@ -1,3 +1,8 @@
+<head>
+    <!--bootstrap.min.css-->
+    <title>Player AMANAH</title>
+    <link rel="stylesheet" href="<?php echo base_url('assets_front'); ?>/assets/css/bootstrap.min.css">
+</head>
 
 <?php
     $data_media = $media->row();
@@ -7,3 +12,5 @@
   <source src="<?php echo base_url('uploads/media/').$data_media->media_file; ?>" type="video/mp4">
 Your browser does not support the video tag.
 </video>
+
+<a class="btn btn-primary btn-sm" href="<?php echo site_url(); ?>"> Kembali </a>
