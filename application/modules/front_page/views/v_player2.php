9df8863642cfb95a127febeb1e74f5e60dbc1af7
spidey354@gmail.com <spidey354@gmail.com>
2025-07-09 15:14:46 +0700

    tab madura

--- application/modules/front_page/views/v_player2.php
+++ application/modules/front_page/views/v_player2.php
@@ -8,10 +8,33 @@
     $data_media = $media->row();
 
 ?>
-<video width="480" height="320" controls>
-  <source src="<?php echo base_url('uploads/media/').$data_media->media_file; ?>" type="video/mp4">
-Your browser does not support the video tag.
-</video>
+<!-- Nav tabs -->
+<ul class="nav nav-tabs" role="tablist">
+  <li class="nav-item">
+    <a class="nav-link active" data-bs-toggle="tab" href="#video" role="tab">Bahasa Indonesia</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" data-bs-toggle="tab" href="#info" role="tab">Bahasa Madura</a>
+  </li>
+</ul>
+
+<!-- Tab panes -->
+<div class="tab-content mt-3">
+  <div class="tab-pane fade show active" id="video" role="tabpanel">
+    <video width="480" height="320" controls>
+      <source src="<?php echo base_url('uploads/media/').$data_media->media_file; ?>" type="video/mp4">
+      Your browser does not support the video tag.
+    </video>
+  </div>
+
+  <div class="tab-pane fade" id="info" role="tabpanel">
+    <video width="480" height="320" controls>
+      <source src="<?php echo base_url('uploads/media/').$data_media->media_file; ?>" type="video/mp4">
+      Your browser does not support the video tag.
+    </video>
+  </div>
+</div>
+
 <br/>
 <br/>
 <a class="btn btn-primary btn-sm" href="<?php echo site_url(); ?>"> Kembali </a>
