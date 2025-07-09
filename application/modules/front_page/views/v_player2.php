2e642acb31264fd8453e9e65f1a2d1be78348f5c
spidey354@gmail.com <spidey354@gmail.com>
2025-07-09 16:06:03 +0700

    ok

--- application/modules/front_page/views/v_player2.php
+++ application/modules/front_page/views/v_player2.php
@@ -28,7 +28,7 @@
 
             <!-- Tab panes -->
             <div class="tab-content mt-3">
-                <div class="tab-pane show active" id="video" role="tabpanel" aria-labelledby="video-tab">
+                <div class="tab-pane active" id="video" role="tabpanel" aria-labelledby="video-tab">
                     <h3>Video Ini Berbahasa Indonesia</h3>
                     <video width="480" height="320" controls preload="metadata" id="v_indo">
                         <source src="<?php echo base_url('uploads/media/').$data_media->media_file; ?>" type="video/mp4">
@@ -36,7 +36,7 @@
                     </video>
                 </div>
 
-                <div class="tab-pane fade" id="info" role="tabpanel" aria-labelledby="info-tab">
+                <div class="tab-pane" id="info" role="tabpanel" aria-labelledby="info-tab">
                     <h3>Video Ini Berbahasa Madura</h3>
                     <video width="480" height="320" controls preload="metadata" id="v_madura">
                         <source src="<?php echo base_url('uploads/media/').$data_media->media_file2; ?>" type="video/mp4">
