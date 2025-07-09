4b24f3db8022477ee5054787503da53f5465a262
spidey354@gmail.com <spidey354@gmail.com>
2025-07-09 16:03:03 +0700

    tabs

--- application/modules/front_page/views/v_player2.php
+++ application/modules/front_page/views/v_player2.php
@@ -2,9 +2,9 @@
     <!-- bootstrap.min.css -->
     <title>Player AMANAH</title>
     <!-- jQuery (required by Bootstrap 4) -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    <script src="https://code.jquery.com/ui/1.14.1/jquery-ui.js"></script>
     <script src="<?php echo base_url('assets_front'); ?>/assets/js/bootstrap.bundle.min.js"></script>
-
     <link rel="stylesheet" href="<?php echo base_url('assets_front'); ?>/assets/css/bootstrap.min.css">
 
 </head>
