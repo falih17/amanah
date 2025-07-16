c506ffc8c3ef28cb279dfd64a4e3a8d923bde5de
Adi Falih <spidey354@gmail.com>
2025-07-16 10:53:46 +0700

    jquery ui

--- application/modules/front_page/views/v_front.php
+++ application/modules/front_page/views/v_front.php
@@ -51,6 +51,11 @@
 			<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
 			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
+		<!-- jQuery UI CSS -->
+		<link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+
+		<!-- jQuery UI -->
+		<script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"></script>
 
     </head>
 	
